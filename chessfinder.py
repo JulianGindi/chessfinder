e36f06b2e48051e36f3fe4fffaa3a0991979b1bb
Julian Gindi <juliangindi@gmail.com>
2016-08-11 22:06:15 -0400

    working horizontal movement for rook movement

--- chessfinder.py
+++ chessfinder.py
@@ -1,16 +1,43 @@
 # ChessFinder
+from typing import Tuple, List
 
 # Custom type to represent a chesspiece index
-BoardIndex = List[int,int]
+BoardIndex = Tuple[int,int]
+Board = List[List[str]]
 
+BOARD_SIZE = 8
 
 class ChessPiece:
-        Pawn, Knight, Bishop, Rook, Queen, King = range(6)
+        WPawn, WKnight, WBishop, WRook, WQueen, WKing, \
+        BPawn, BKnight, BBishop, BRook, BQueen, BKing = range(1,13)
+
+
+def pawn(piece_color: ChessPiece):
+    if piece_color is ChessPiece.BPawn:
+        return lambda x: (x[0] + 1, x[1])
+    elif piece_color is ChessPiece.WPawn:
+        return lambda x: (x[0] - 1, x[1])
+
+def rook(piece_color: ChessPiece):
+    if piece_color is ChessPiece.BRook:
+        def f(piece_index):
+            v_moves = map(lambda x: (piece_index[0] + x, piece_index[1]), range(1, BOARD_SIZE))
+            return list(v_moves)
+        return f
+    elif piece_color is ChessPiece.WRook:
+        def f(piece_index):
+            v_moves = map(lambda x: (piece_index[0] + x, piece_index[1]), range(-1, -BOARD_SIZE, -1))
+            return list(v_moves)
+        return f
 
 
 def is_legal_move(start: BoardIndex, end: BoardIndex, piece_type: ChessPiece) -> bool:
     pass
 
 
-def return_pieace_at_location(loc: BoardIndex) -> ChessPiece:
+def return_piece_at_location(loc: BoardIndex, board: Board) -> ChessPiece:
+    return board[loc[0]][loc[1]]
+
+
+def spaces_to_next_piece(loc: BoardIndex, board: Board) -> Tuple[int, int]:
     pass
--- chessfinder_tests.py
+++ chessfinder_tests.py
@@ -1,21 +1,49 @@
 # Chess Tests
 import unittest
 
+from chessfinder import *
+
 TEST_BOARD = [
-    ['r-b','k-b','b-b','q-b','k-b','b-b','k-b','r-b'],
-    ['p-b','p-b','p-b','p-b','p-b','p-b','p-b','p-b'],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['r-w','k-w','b-w','q-w','k-w','b-w','k-w','r-w'],
-    ['p-w','p-w','p-w','p-w','p-w','p-w','p-w','p-w'],
+    [10,8,9,11,12,9,8,10],
+    [7,7,7,7,7,7,7,7],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1],
+    [4,2,3,5,6,3,2,4]
 ]
 
 class TestChessFinder(unittest.TestCase):
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_return_piece_at_location(self):
+        expected = ChessPiece.BBishop
+        self.assertEqual(return_piece_at_location((0, 2), TEST_BOARD), expected)
+
+    def test_black_pawn_movement(self):
+        expected = (2, 0)
+        piece_index = (1, 0)
+        pawn_color = return_piece_at_location(piece_index, TEST_BOARD)
+        self.assertEqual(pawn(pawn_color)(piece_index), expected)
+
+    def test_white_pawn_movement(self):
+        expected = (5, 0)
+        piece_index = (6, 0)
+        pawn_color = return_piece_at_location(piece_index, TEST_BOARD)
+        self.assertEqual(pawn(pawn_color)(piece_index), expected)
+
+    def test_black_rook_movement(self):
+        expected = [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)]
+        piece_index = (0, 0)
+        pawn_color = return_piece_at_location(piece_index, TEST_BOARD)
+        self.assertEqual(rook(pawn_color)(piece_index), expected)
+
+    def test_white_rook_movement(self):
+        expected = [(6, 0), (5, 0), (4, 0), (3, 0), (2, 0), (1, 0), (0, 0)]
+        piece_index = (7, 0)
+        pawn_color = return_piece_at_location(piece_index, TEST_BOARD)
+        self.assertEqual(rook(pawn_color)(piece_index), expected)
 
 if __name__ == '__main__':
     unittest.main()
