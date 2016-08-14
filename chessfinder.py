d491457b1b9aa7f8469521898fcecc349ad1705a
Julian Gindi <juliangindi@gmail.com>
2016-08-14 16:48:12 -0400

    working knight, refactor

--- chessfinder.py
+++ chessfinder.py
@@ -15,23 +15,30 @@ class ChessPiece:
 def king():
     def f(piece_index):
         movement = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]
-        moves = map(lambda x: (piece_index[0] + x[0], piece_index[1] + x[1]), movement)
-        moves = filter_invalid_moves(moves, piece_index)
+        moves = add_movement_to_piece(piece_index, movement)
         return list(moves)
     return f
 
 
 def pawn(piece_color: ChessPiece):
-    if piece_color is ChessPiece.BPawn:
-        return lambda x: (x[0] + 1, x[1])
-    elif piece_color is ChessPiece.WPawn:
-        return lambda x: (x[0] - 1, x[1])
+    def f(piece_index):
+        movement = [(1, 0)]
+        # If pawn is in starting file, it can move two spaces as well
+        if piece_index[0] is 6 or piece_index[0] is 1:
+            movement.append((2, 0))
+        if piece_color is ChessPiece.BPawn:
+            return map(lambda x: (piece_index[0] + x[0], piece_index[1]), movement)
+        elif piece_color is ChessPiece.WPawn:
+            return map(lambda x: (piece_index[0] - x[0], piece_index[1]), movement)
+    return f
 
 
 def rook():
     def f(piece_index):
-        v_moves = map(lambda x: (piece_index[0] + x, piece_index[1]), range(-BOARD_SIZE, BOARD_SIZE, 1))
-        h_moves = map(lambda x: (piece_index[0], piece_index[1] + x), range(-BOARD_SIZE, BOARD_SIZE, 1))
+        v_moves = map(lambda x: (piece_index[0] + x, piece_index[1]),
+                      range(-BOARD_SIZE, BOARD_SIZE, 1))
+        h_moves = map(lambda x: (piece_index[0], piece_index[1] + x),
+                      range(-BOARD_SIZE, BOARD_SIZE, 1))
         v_moves = filter_invalid_moves(v_moves, piece_index)
         h_moves = filter_invalid_moves(h_moves, piece_index)
         return list(v_moves) + list(h_moves)
@@ -40,11 +47,21 @@ def rook():
 
 def bishop():
     def f(piece_index):
-        moves_a = map(lambda x: (piece_index[0] - x, piece_index[1] + x), range(-BOARD_SIZE, BOARD_SIZE, 1))
-        moves_b = map(lambda x: (piece_index[0] - x, piece_index[1] - x), range(-BOARD_SIZE, BOARD_SIZE, 1))
+        moves_a = map(lambda x: (piece_index[0] - x, piece_index[1] + x),
+                      range(-BOARD_SIZE, BOARD_SIZE, 1))
+        moves_b = map(lambda x: (piece_index[0] - x, piece_index[1] - x),
+                      range(-BOARD_SIZE, BOARD_SIZE, 1))
         moves_a = filter_invalid_moves(moves_a, piece_index)
         moves_b = filter_invalid_moves(moves_b, piece_index)
         return moves_a + moves_b
+    return f
+
+def knight():
+    def f(piece_index):
+        movement = [(2, 1), (2, -1), (-2, 1), (-2, -1),
+                    (0, 0), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+        moves = add_movement_to_piece(piece_index, movement)
+        return list(moves)
     return f
 
 def queen():
@@ -54,8 +71,16 @@ def queen():
         return moves_a + moves_b
     return f
 
+def add_movement_to_piece(piece_index, movement):
+    m = map(lambda x: (piece_index[0] + x[0], piece_index[1] + x[1]), movement)
+    m = filter_invalid_moves(m, piece_index)
+    return m
+
+
 def filter_invalid_moves(moves, pos):
-    move_list = list(filter(lambda x: x[0] >= 0 and x[0] < BOARD_SIZE and x[1] >= 0 and x[1] < BOARD_SIZE, moves))
+    move_list = list(filter(
+        lambda x: x[0] >= 0 and x[0] < BOARD_SIZE and x[1] >= 0 and x[1] < BOARD_SIZE, moves
+    ))
     move_list.remove(pos)
     return move_list
 
--- chessfinder_tests.py
+++ chessfinder_tests.py
@@ -23,22 +23,27 @@ class TestChessFinder(unittest.TestCase):
         self.assertEqual(return_piece_at_location((0, 2), TEST_BOARD), expected)
 
     def test_black_pawn_movement(self):
-        expected = (2, 0)
+        expected = [(2, 0), (3, 0)]
         piece_index = (1, 0)
         pawn_color = return_piece_at_location(piece_index, TEST_BOARD)
-        self.assertEqual(pawn(pawn_color)(piece_index), expected)
+        self.assertCountEqual(pawn(pawn_color)(piece_index), expected)
 
     def test_white_pawn_movement(self):
-        expected = (5, 0)
+        expected = [(5, 0), (4, 0)]
         piece_index = (6, 0)
         pawn_color = return_piece_at_location(piece_index, TEST_BOARD)
-        self.assertEqual(pawn(pawn_color)(piece_index), expected)
+        self.assertCountEqual(pawn(pawn_color)(piece_index), expected)
 
     def test_rook_movement(self):
         expected = [(0, 2), (1, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2),
                     (2, 0), (2, 1), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)]
         piece_index = (2, 2)
         self.assertCountEqual(rook()(piece_index), expected)
+
+    def test_knight_movement(self):
+        expected = [(5, 5), (5, 7), (6, 4)]
+        piece_index = (7, 6)
+        self.assertCountEqual(knight()(piece_index), expected)
 
     def test_bishop_movement(self):
         expected = [(7, 2), (6, 3), (4, 5), (3, 6), (2, 7),
